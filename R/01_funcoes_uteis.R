64bae1483b2c8781d845c27022dda4ee25ab289a
Victor-Vieira01 <155432873+Victor-Vieira01@users.noreply.github.com>
2024-01-04 10:02:55 +0000

    Funções uteis

---
+++ R/01_funcoes_uteis.R
@@ -0,0 +1,7 @@
+# Função util para conexão com banco de dados SQL
+conectar_sql <- function(conexao = duckdb::duckdb(), local) {
+    DBI::dbConnect(
+    drv = conexao,
+    dbdir = local
+    )
+}
